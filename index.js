fedd2b90d3b1a38701c3122b6aa5b4538e921909
azoladyantyi <aizydyantyi@gmail.com>
2017-10-02 16:54:31 +0200

    updates

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
+const express = require('express');
 const exphbs = require("express-handlebars");
 const form = require('body-parser');
-var express = require('express');
-const mongoURL = process.env.MONGO_DB_URL || "mongodb://localhost/waiterApp";
-const models = require("./models");
-
+const flash = require('express-flash');
+const session = require('express-session');
+const bodyParser = require('body-parser');
+const mongoURL = process.env.MONGO_DB_URL || "mongodb://localhost/greetings";
+const Models = require("./models");
 
 var app = express();
-var app = express();
-
+app.use(bodyParser.json());
+app.use(session({
+    secret: 'keyboard cat',
+    cookie: {
+        maxAge: 60000 * 30
+    }
+}));
+app.use(flash());
 app.engine("handlebars", exphbs({
     defaultLayout: "main",
     extname: "handlebars"
@@ -28,6 +36,29 @@ app.get('/waiter/:username', function(req, res) {
 
 
     res.render("home",{
+      displayUsername: username
+    })
+});
+app.post('/waiter/:username', function(req, res) {
+  var username = req.params.username;
+Mode.waiterData.find({}, function(err, results) {
+  if (err) {
+    return next(err)
+  }else {
+
+    res.render("home",{
+      displayUsername: results
+    })
+  }
+})
+
+});
+
+app.post('/waiter/:username', function(req, res) {
+  var username = req.params.username;
+
+
+    res.render("admin",{
       displayUsername: username
     })
 });
